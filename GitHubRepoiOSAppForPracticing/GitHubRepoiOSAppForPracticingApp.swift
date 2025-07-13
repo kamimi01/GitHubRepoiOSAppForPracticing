67007e0372c075c1de6d4660704ec2058746d72d
kamimi01 <awakaru0122@gmail.com>
2025-07-13 11:52:39 +0200

    Initial Commit

---
+++ GitHubRepoiOSAppForPracticing/GitHubRepoiOSAppForPracticingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepoiOSAppForPracticingApp.swift
+//  GitHubRepoiOSAppForPracticing
+//
+//  Created by mika_admin on 2025-07-13.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepoiOSAppForPracticingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
